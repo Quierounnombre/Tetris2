911c4ddb25fcbb8a930da18d2e7449f16c57dfd4
root <root@DESKTOP-AHR157M.localdomain>
2022-12-14 23:36:54 +0100

    piezan caen, necesitan barreras

--- scripts/PlayerControls.cs
+++ scripts/PlayerControls.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     public GameManager 	GameManager;
-    public Vector2Int			pos;
-
-	private void Awake() 
-	{
-		pos = new Vector2Int(5, 10);
-	}
+	public board		board;
+	public piece		piece;
+    public Vector2Int	pos;
+    public float        timedelay;
+    private float       deltatime;
+    
 
     void Update()
     {
-        if(Input.GetButton("Cancel"))
-        {
-            GameManager.instance.pause();
-        }
+		if (Time.time >= deltatime)
+			Drop();
+    }
+
+	public void Awake()
+	{
+		piece = board.gen.generate();
+		deltatime = Time.time + timedelay;
+		board.spawn_piece(piece);
+	}
+
+	public void Drop()
+	{
+		deltatime = Time.time + timedelay;
+		board.clean_piece(piece);
+		pos -= new Vector2Int(0, 1);
+		board.Move_piece(piece);
     }
 }
--- scripts/board.cs
+++ scripts/board.cs
@@ -9,12 +9,8 @@ public class board : MonoBehaviour
 	public piece_generator gen;
 	private Tile tile;
 	public PlayerControls player;
-	private void Start() {
-		piece p = gen.generate();
-		Move_piece(p);
-	}
+	public Vector2Int spawn_point;
 
-	
     public void Move_piece(piece piece)
     {
 		Vector2Int tilepos;
@@ -22,10 +18,33 @@ public class board : MonoBehaviour
         for (int i = 0; i < piece.cells.Length; i++)	
         {
 			tilepos = piece.cells[i] + player.pos;
-			Debug.Log((Vector3Int)tilepos);
 			tilemap.SetTile((Vector3Int)tilepos, piece.tile);
         }
-		Debug.Log("He terminado");
     }
+
+	public void clean_piece(piece piece)
+	{
+		Vector2Int tilepos;
+
+        for (int i = 0; i < piece.cells.Length; i++)	
+        {
+			tilepos = piece.cells[i] + player.pos;
+			tilemap.SetTile((Vector3Int)tilepos, null);
+        }
+	}
+
+	public void spawn_piece(piece piece)
+	{
+		Vector2Int tilepos;
+
+		for (int i = 0; i < piece.cells.Length; i++)
+		{
+			tilepos = piece.cells[i] + spawn_point;
+			if (tilemap.GetTile((Vector3Int)tilepos) != null)
+				Time.timeScale=0; // GAMEOVER IN FUTURE
+		}
+		player.pos = spawn_point;
+		Move_piece(piece);
+	}
 	
 }
