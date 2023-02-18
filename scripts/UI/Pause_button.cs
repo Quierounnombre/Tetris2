4710479eb4c479c98844134b25d353a3170bc110
root <root@DESKTOP-AHR157M.localdomain>
2023-02-18 16:37:53 +0100

    Pause WIP

--- scripts/UI/Pause_button.cs
+++ scripts/UI/Pause_button.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Pause_button : MonoBehaviour
 {
-    public GameManager  gm;
-    public bool         is_pause;
-    public Color        pulsed_color;
-    public Color        clean_color;
-    public Pause_button other_button;
+    public GameManager  	gm;
+    public bool        	 	is_pause;
+    public Color        	pulsed_color;
+    public Color        	clean_color;
+    public Pause_button 	other_button;
+    private SpriteRenderer	Sr;
+
+    private void Awake()
+	{
+        Sr = gameObject.GetComponent<SpriteRenderer>();
+    }
 
     private void OnMouseDown()
     {
         if (is_pause && Time.timeScale == 1)
         {
-            gameObject.SpriteRenderer.color = pulsed_color;
-            other_button.SpriteRenderer.color = clean_color;
+    		Sr.color = pulsed_color;
+            other_button.Sr.color = clean_color;
             gm.pause();
         }
         else if (!is_pause && Time.timeScale == 0)
         {
-            gameObject.SpriteRenderer.color = pulsed_color;
-            other_button.SpriteRenderer.color = clean_color;
+            Sr.color = pulsed_color;
+            other_button.Sr.color = clean_color;
             gm.resume();
         }
     }
